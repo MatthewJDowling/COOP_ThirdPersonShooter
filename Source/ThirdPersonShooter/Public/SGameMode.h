458735698de65dbb10f995b654a0f28b39f84736
Matthew Dowling <s189051@students.aie.edu>
2018-12-06 16:46:59 -0800

    Begun spawn bots on wave system

--- Source/ThirdPersonShooter/Public/SGameMode.h
+++ Source/ThirdPersonShooter/Public/SGameMode.h
@@ -15,6 +15,35 @@ class THIRDPERSONSHOOTER_API ASGameMode : public AGameModeBase
 	GENERATED_BODY()
 
 
+protected:
 
+	FTimerHandle TimerHandle_BotSpawner;
+
+	int32 NrOfBotsToSpawn;
+
+	int32 WaveCount;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+	float TimeBetweenWaves;
+
+
+
+	//Hook for BP to spawn a single bot
+	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+	void SpawnNewBot();
+
+	void SpawnBotTimerElapsed();
+
+	void StartWave();
+
+	void EndWave();
+
+	void PrepareForNextWave();
+
+public:
+
+	ASGameMode();
+
+	virtual void StartPlay() override;
 	
 };
